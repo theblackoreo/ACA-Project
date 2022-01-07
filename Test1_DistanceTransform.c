9bd89dcc4b22eb0348e19e5c1ef8717faf067f23
francescomarins <80126256+francescomarins@users.noreply.github.com>
2022-01-07 17:33:27 +0100

    Prova

--- Test1_DistanceTransform.c
+++ Test1_DistanceTransform.c
@@ -1,18 +1,20 @@
 // distance trasnform between 2 points in a matrix of ones and zeros (simulate binary image)
 
 #include <stdio.h> 
+#include <stdlib.h>
 
 int A[100][100];
 
-
-
 int main(){
-
-    for(int j = 0; j < 100; j++){
-        for(int i = 0; i < 100; i++){
-            if(i%2 != 0){ 
-                A[i][j] = 1;
-            }
+    for(int i = 0; i < 100; i++){
+        for(int j = 0; j < 100; j++){
+            A[i][j] = rand()%2;
         }
+    }
+	 for(int i = 0; i < 100; i++){
+        for(int j = 0; j < 100; j++){
+            printf("%d ", A[i][j]);
+        }
+		prinftf("\n");
     }
 }
