f2b24d4fed7d6d1d21c03d1327a61f8bc48d20ad
theblackoreo <fabio.tagliani03@universitadipavia.it>
2022-01-08 18:05:38 +0100

    test

--- Test1_DistanceTransform.cpp
+++ Test1_DistanceTransform.cpp
@@ -1,27 +1,48 @@
-// distance trasnform between 2 points in a matrix of ones and zeros (simulate binary image)
+#include <iostream>
 
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
-
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
 #define N 20
 
 int A[N][N];
+int dilation_size = 1;
+
+using namespace cv;
+using namespace std;
 
 int main(){
     for(int i = 0; i < N; i++){
         for(int j = 0; j < N; j++){
             A[i][j] = rand()%2;
-			if (i == j)
-				A[i][j] = 1;
+            if (i == j)
+                A[i][j] = 1;
         }
     }
-	 for(int i = 0; i < N; i++){
+    for(int i = 0; i < N; i++){
         for(int j = 0; j < N; j++){
-            printf("%d ", A[i][j]);
+           // printf("%d ", A[i][j]);
         }
-		printf("\n");
+      //  printf("\n");
     }
 
-    //test
+    cv::Mat B(20, 20, CV_64F, A);
+  // std::memcpy(B.data, A, 20*20*sizeof(double));
+
+    cv::Mat C(20, 20, CV_64F, A);
+
+
+
+        cv::Mat element = getStructuringElement( MORPH_RECT,
+                                             Size( 2*dilation_size + 1, 2*dilation_size+1 ),
+                                             Point( dilation_size, dilation_size ) );
+        dilate(B, C, MORPH_RECT);
+        imshow( "Dilation Demo", C );
+
+
+
+
+
 
 }
