5cdaf3e22eedc4a9439667e6c2fd23e9cfabcc58
Francesco Marinelli <francesco.marinelli@outlook.com>
2022-01-26 18:08:06 +0100

    error on the border inspection

--- failure1.cpp
+++ failure1.cpp
@@ -14,47 +14,85 @@ using namespace cv;
 const int MAXBYTES=8*1024*1024;
 uchar buffer[MAXBYTES];
 uchar buffer_to_recv[MAXBYTES];
+uchar buffer_to_send[MAXBYTES];
 int my_rank, size, rows, cols, bytes;
 Mat src;
 int dims[2];
 
-void matscatter(Mat& m, int my_rank){
+// direction vectors
+const int dx[] = {+1, 0, -1, 0};
+const int dy[] = {0, +1, 0, -1};
 
-if(my_rank == 0){
+void checkNearByte(int current_label, int r, int c, Mat& src, Mat& label_dst) {
+  rows = src.rows;
+  cols = src.cols;
+  if (r < 0 || r == rows) return; // out of bounds
+  if (c < 0 || c == cols) return; // out of bounds
+  if (label_dst.at<unsigned char>(r,c) || !src.at<unsigned char>(r,c)) return; // already labeled or not marked with 1 in src
 
-  cols = m.cols;
+  // mark the current cell
+  label_dst.at<unsigned char>(r,c) = current_label;
 
-  int bytespersample=1; // change if using shorts or floats
-  bytes=m.rows*m.cols*bytespersample;
-
-  cout << "matsnd: bytes=" << bytes << endl;
-  cout << endl << m << endl << endl;
-  if(!m.isContinuous())
-  {
-    m = m.clone();
-  }
-
-  memcpy(&buffer[0*sizeof(int)],m.data,bytes);
-
-  printf("to send: ");
-  for (int i = 0; i < bytes; i++){
-    printf("%hhu ", buffer[i]);
-  }
-
-  dims[0] = rows/size;
-  dims[1] = cols;
-  //bytes = bytes/size;
-
-  for(int i = 1; i < size; i++){
-    MPI_Send(&dims,2*sizeof(int),MPI_INT,i,555, MPI_COMM_WORLD);
-  }
-
+  // recursively mark the neighbors
+  for (int direction = 0; direction < 4; ++direction)
+  checkNearByte(current_label, r + dx[direction], c + dy[direction], src, label_dst);
 }
-  else {
-  MPI_Status status;
-  MPI_Recv(&dims,2*sizeof(int),MPI_INT,0,555, MPI_COMM_WORLD, &status);
 
-  printf("RICEVUTO %d\n", dims[0]*dims[1]);
+//function to identify different regions of the labirinth
+Mat find_components(Mat& rcv, Mat& label_dst) {
+  int component = 0;
+  for (int i = 0; i < rcv.rows; i++)
+  for (int j = 0; j < rcv.cols; j++)
+  if (!label_dst.at<unsigned char>(i,j) && rcv.at<unsigned char>(i,j)) {
+    checkNearByte(++component, i, j, rcv, label_dst);
+    printf("Sono entrato con i=%d e j=%d mentre label=%d\n", i, j, component);
+  }
+  imshow("LABELLED MAT TO RETURN", label_dst*50);
+  return label_dst;
+}
+
+
+void matscatter(Mat& m, int my_rank){
+  if(my_rank == 0){
+    cols = m.cols;
+
+    int bytespersample=1; // change if using shorts or floats
+    bytes=m.rows*m.cols*bytespersample;
+
+    cout << "matsnd: bytes=" << bytes << endl;
+    cout << endl << m << endl << endl;
+    if(!m.isContinuous())
+    {
+      m = m.clone();
+    }
+
+    memcpy(&buffer[0*sizeof(int)],m.data,bytes);
+
+    printf("to send: ");
+    for (int i = 0; i < bytes; i++){
+      printf("%hhu ", buffer[i]);
+    }
+
+    dims[0] = (rows/size);
+    dims[1] = cols;
+    //bytes = bytes/size;
+
+    for(int i = 1; i < size; i++){
+      MPI_Send(&dims,2*sizeof(int),MPI_INT,i,555, MPI_COMM_WORLD);
+      /*
+      memcpy(&buffer_to_send[0*sizeof(int)],m.data+160*161/size, 160*161/size);
+      Mat mat_to_snd = Mat(80, 161,0,buffer_to_send);
+      imshow("mat to send", mat_to_snd);
+      waitKey(0);
+      */
+    }
+
+  }
+  else {
+    MPI_Status status;
+    MPI_Recv(&dims,2*sizeof(int),MPI_INT,0,555, MPI_COMM_WORLD, &status);
+
+    printf("RICEVUTO %d\n", dims[0]*dims[1]);
 
   }
 
@@ -62,26 +100,23 @@ if(my_rank == 0){
   rows = dims[0];
   cols = dims[1];
 
-
   MPI_Scatter(&buffer, bytes, MPI_UNSIGNED_CHAR, buffer_to_recv, bytes, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
 
 
-  Mat a = Mat(rows,cols,0,buffer_to_recv);
-
-  cout << endl << a << endl << endl;
-  /*
-  for (int i = 0; i < bytes; i++){
-    printf("process: %d, element n. %d ---> %hhu\n", my_rank, i, buffer_to_recv[i]);
-  }
-  */
+  // for (int i = 0; i < bytes; i++){
+  // printf("process: %d, element n. %d ---> %hhu\n", my_rank, i, buffer_to_recv[i]);
+  // }
 
 }
 
-Mat reconstruct(){
-  // Make the mat
-  Mat received= Mat(1,0,0,(uchar*)&buffer[0*sizeof(int)]);
-
-  return received;
+void matgather(Mat& m) {
+  memcpy(&buffer_to_recv[0*sizeof(int)],m.data,bytes);
+  MPI_Gather(&buffer_to_recv, bytes, MPI_UNSIGNED_CHAR, buffer, bytes, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
+  if(my_rank == 0) {
+    m = Mat(rows*size,cols,0,buffer);
+    imshow("Returned", m*255);
+    waitKey(0);
+  }
 }
 
 int main(int argc, char* argv[])
@@ -98,7 +133,6 @@ int main(int argc, char* argv[])
   {
     int i, j, element_size, quattro, mode;
     mode = atoi(argv[2]) + 1 ;
-    quattro = 4;
 
     if(mode<2 || mode>3) {
       fprintf(stderr, "Wrong mode: 1 or 2\n");
@@ -106,20 +140,61 @@ int main(int argc, char* argv[])
     }
 
     src = imread(argv[1], IMREAD_REDUCED_GRAYSCALE_2);
+    int height = src.rows - (src.rows%size);
+    int width = src.cols;
+    resize(src, src, Size(width, height));
     rows = src.rows;
     cols = src.cols;
+    // printf("%d x %d\n", src.rows, src.cols);
+    // getchar();
+    //cout << endl << src << endl << endl;
+    imshow("Resized", src);
+    waitKey(0);
+  }
 
-    resize(src, src, Size(rows-(rows%size), cols));
-    rows = rows - (rows%size);
+  matscatter(src, my_rank);
+  Mat received = Mat(rows,cols,0,buffer_to_recv);
+  //cout << endl << received << endl << endl;
+
+  //We tranform the matrix so that we have 0s and 1s.
+  threshold(received, received, 128,1,THRESH_BINARY);
+  //printf("Process %d: \n", my_rank);
+
+
+  // Complement of the received matrix
+  for(int i = 0; i < received.rows; i++){
+    for(int j = 0; j < received.cols; j++){
+      received.at<unsigned char>(i,j) = (received.at<unsigned char>(i,j) - 1) * (-1);
+    }
+
+    //Printing the thresholded and complemented matrix
+    // cout << endl << received << endl << endl;
+    // imshow("Thresholded and complemented", received*255);
+    // waitKey(0);
+  }
+
+  matgather(received);
+  // imshow("Received in main", received*50);
+  // waitKey(0);
+
+  if(my_rank == 0) {
+    Mat label_dst = Mat::zeros(received.rows, received.cols, CV_8UC1);
+    label_dst = find_components(received, label_dst);
+    imshow("Components", label_dst*50);
+    waitKey(0);
+
+    // uchar up[MAXBYTES];
+    // memcpy(&up[], label_dst.rowRange(0, 0).data, label_dst.cols);
+    // std::vector<uchar>down;
+    // memcpy(&down, label_dst.rowRange(label_dst.rows-1, label_dst.rows-1).data, label_dst.cols);
+    // std::vector<uchar> right;
+    // memcpy(&right, label_dst.colRange(label_dst.cols-1, label_dst.cols-1).data, label_dst.rows);
+    // std::vector<uchar> left;
+    // memcpy(&left, label_dst.colRange(0, 0).data, label_dst.rows);
 
 
 
   }
-
-  matscatter(src, my_rank);
-
-//  Mat m = reconstruct();
-
 
   /* Terminate MPI */
   MPI_Finalize();
